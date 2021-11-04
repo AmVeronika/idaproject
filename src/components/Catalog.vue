269f401fa0339ab982dec1eca0cb1bbc6afcaf86
Veronika Mikhotina <mihotinaw@gmail.com>
2021-11-04 23:51:07 +0300

    creation components withgoods

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>Catalog</div>
+  <div :class="$style.catalog">
+    <selectCustom></selectCustom>
+    <GoodsCard></GoodsCard>
+  </div>
+
 </template>
 
 <script>
+import { mapActions } from "vuex";
+import GoodsCard from "./GoodsCard";
+import selectCustom from "./elements/selectCustom";
+
 export default {
   name: "Catalog",
+  components: {
+    GoodsCard,
+    selectCustom
+  },
+  methods: {
+    ...mapActions(["fetchDataGoodsList"]),
+  },
+  mounted() {
+    this.fetchDataGoodsList();
+  },
 };
 </script>
 
-<style scoped></style>
-
-
+<style lang="scss" module>
+.catalog {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-between;
+  gap: 16px;
+}
+</style>
